e8856b2ad67ca10fc96e16c28b4bec58d66b34cb
vasiliy <shabanovfedor@yandex.ru>
2019-05-13 19:55:20 +0300

    76575

--- src/ru/fedor/Main.java
+++ src/ru/fedor/Main.java
@@ -3,6 +3,6 @@ package ru.fedor;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("commit6456456745");
+	    System.out.println("commit64564567lk;kllk;k45");
     }
 }
