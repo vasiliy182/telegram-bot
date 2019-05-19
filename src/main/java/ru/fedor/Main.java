5d19dc4ec7ba1be9e8371f48b35ffe8d2625e0fd
vasiliy182 <50590847+vasiliy182@users.noreply.github.com>
2019-05-19 19:25:38 +0300

    Add files via upload

--- src/main/java/ru/fedor/Main.java
+++ src/main/java/ru/fedor/Main.java
@@ -3,5 +3,14 @@ package ru.fedor;
 public class Main {
     public static void main(String[] args) {
 
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new MyAmazingBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/ru/fedor/MyBot.java
+++ src/main/java/ru/fedor/MyBot.java
@@ -8,6 +8,14 @@ public class MyBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+        try {
+            execute(message); // Call method to send the message
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
 
     }
 
@@ -18,6 +26,6 @@ public class MyBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        return null;
+        return "я знаю, что здесь долженбыть токен";
     }
 }
